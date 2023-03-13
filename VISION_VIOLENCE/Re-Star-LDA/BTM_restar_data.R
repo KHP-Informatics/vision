672da25c9e694978840666f1a36c3f1c1cecc43d
Lifang Li <lifangli2c@gmail.com>
2023-03-13 20:27:08 +0000

    Update BTM_restar_data.R

--- VISION_VIOLENCE/Re-Star-LDA/BTM_restar_data.R
+++ VISION_VIOLENCE/Re-Star-LDA/BTM_restar_data.R
@@ -1,7 +1,7 @@
 library(data.table)
 library(udpipe)
 ## Annotate text with parts of speech tags
-data <- read.csv("/Users/lilifang/KCL/KCL_Angus/RE-STAR-Angus-Susie/All interviewees responses_withoutNONE_updatedV14_with_BTM_b.csv",header=TRUE,sep=",",na.strings = NULL)
+data <- read.csv("/Users/lilifang/KCL/KCL_Angus/RE-STAR-Angus-Susie/All interviewees responses_withoutNONE_updatedV17_with_BTM_b.csv",header=TRUE,sep=",",na.strings = NULL)
 #data <- data[0:1000,]
 data<- data[, c("ids", "new_response")]
 dim(data)
@@ -14,13 +14,13 @@ anno <- udpipe(anno, "english", trace = 500)
 
 biterms <- as.data.table(anno)
 biterms <- biterms[, cooccurrence(x = lemma,
-                                  relevant =  nchar(lemma) > 2 & !lemma %in% stopwords("en"), #upos %in% c("NOUN", "ADJ", "VERB") &
+                                  relevant =  nchar(lemma) > 2 & !lemma %in% stopwords("en"), 
                                   skipgram = 3),
                    by = list(doc_id)]
 
 library(BTM)
 set.seed(1234)
-traindata <- subset(anno,  !lemma %in% stopwords("en") & nchar(lemma) > 2) # upos %in% c("NOUN", "ADJ", "VERB") &
+traindata <- subset(anno,  !lemma %in% stopwords("en") & nchar(lemma) > 2) 
 traindata <- traindata[, c("doc_id", "lemma")]
 model     <- BTM(traindata, biterms = biterms, k = 10, iter = 500, background = TRUE, trace = 100)
 
